7d9cb3acb2a32e6efebb3d543466b8bb1a0fa90b
DV <github email address>
2017-06-02 18:46:11 +0900

    Add bin and config structures, and add basic initilization of audiobooks

--- lib/audiobook.rb
+++ lib/audiobook.rb
@@ -1,10 +1,19 @@
 class Audiobook
   @@all = Array.new
 
+  def self.mass_initialize(url_librivox_hash_array)
+    url_librivox_hash_array.each{ |url_librivox| Audiobook.new(url_librivox) }
+  end
+
+  def self.all()
+    return @@all
+  end
+
   attr_accessor :url_librivox
 
   def initialize(url_librivox)
-    add_attributes(url_librivox)
+    self.add_attributes(url_librivox)
+    @@all << self
   end
 
   def add_attributes(attributes)
--- lib/collection.rb
+++ lib/collection.rb
@@ -1,9 +1,20 @@
+require 'open-uri'
+require 'json'
+require_relative 'audiobook'
+
 class Collection
 
   LIBRIVOX_API_URL = "https://librivox.org/api/feed/audiobooks/"
-  LIBRIVOX_CATALOG_SIZE = "2000"
+  LIBRIVOX_CATALOG_SIZE = "20"
   LIBRIVOX_API_CALL_FOR_AUDIOBOOK_URLS = LIBRIVOX_API_URL +
                 "?fields={url_librivox}&format=json&limit=#{LIBRIVOX_CATALOG_SIZE}"
 
+  def initialize()
+    json_string = open(LIBRIVOX_API_CALL_FOR_AUDIOBOOK_URLS).read
+    url_librivox_hash_array = JSON.parse(json_string,{symbolize_names: true}).values[0]
+    Audiobook.mass_initialize(url_librivox_hash_array)
+  end
 
 end
+
+Collection.new
