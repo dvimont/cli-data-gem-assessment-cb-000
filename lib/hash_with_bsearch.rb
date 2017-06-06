50d15093eee908050d67590ccfb517df3a2051a3
DV <github email address>
2017-06-06 12:15:53 +0900

    added comments at top of HashWithBsearch

--- lib/hash_with_bsearch.rb
+++ lib/hash_with_bsearch.rb
@@ -1,3 +1,10 @@
+## NOTE: This subclass of Hash efficiently returns values (via the #[] method)
+##       through encapsulated use of the Array#bsearch method.
+##  It is intended for situations in which efficiency of lookups is paramount,
+##  but where efficiency in adding values and building the initial Hash is not
+##  so important. Note that every time the content of the Hash is altered, it
+##  requires the @sorted_key_value_array to be computed via hash#sort.
+
 class HashWithBsearch < Hash
 
   @sorted_key_value_array = Array.new
@@ -10,7 +17,23 @@ class HashWithBsearch < Hash
     end
   end
 
-  # OVERRIDES
+  # OVERRIDES OF ADD and GET ("[bracket]") methods
+  def []=(key, value)
+    result = super
+    self.sync_sorted_key_value_array
+    return result
+  end
+
+  def [](key)
+    if @sorted_key_value_array.empty?
+      return nil
+    else
+      found_kv_pair = @sorted_key_value_array.bsearch{|kv_pair| kv_pair[0] >= key}
+      return (found_kv_pair == nil) ? nil : found_kv_pair[1]
+    end
+  end
+
+  # OVERRIDES OF METHODS WHICH RESULT IN ALTERATION OF HASH CONTENT
   def initialize(object=nil)
     result = super
     self.sync_sorted_key_value_array
@@ -27,20 +50,5 @@ class HashWithBsearch < Hash
     result = super
     self.sync_sorted_key_value_array
     return result
-  end
-
-  def []=(key, value)
-    result = super
-    self.sync_sorted_key_value_array
-    return result
-  end
-
-  def [](key)
-    if @sorted_key_value_array.empty?
-      return nil
-    else
-      found_kv_pair = @sorted_key_value_array.bsearch{|kv_pair| kv_pair[0] >= key}
-      return (found_kv_pair == nil) ? nil : found_kv_pair[1]
-    end
   end
 end
